afc05926e603021aeb3dc1122629cc22fbac555f
Jeremy Nagel <jeremy.nagel@learnosity.com>
2016-07-15 08:51:14 +1000

    Initial commit

---
+++ assets/js/coverageCapital.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    addUpdateDataButtonClickListener();
+    hideVideoUntilModalOpened();
+    hideVideoWhenModalClosed();
+});
+
+function addUpdateDataButtonClickListener() {
+    $("button").click(function() {
+        playPowerDownEffect();
+        $(".sec-padding").removeClass("sec-padding");
+        $("section").addClass("animate-effect");
+    });
+}
+
+function playPowerDownEffect() {
+    var audio = new Audio('./assets/sound/Power-down-sound-effect.mp3');
+    audio.volume = 0.5;
+    audio.play();
+}
+
+function hideVideoUntilModalOpened() {
+    var url = $("#video-hack").attr('src');
+    $("#video-hack").attr('src', '');
+    $("#videoModal").on('shown.bs.modal', function() {
+        $("#video-hack").attr('src', url);
+    });
+}
+
+function hideVideoWhenModalClosed() { 
+    $("#videoModal").on('hide.bs.modal', function(){
+        $("#video-hack").attr('src', '');
+    });
+}
